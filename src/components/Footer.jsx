fdc9b9c43ea44460724f88ca71e83d85a6d47458
Diana Martine Matias Pontes <Diana_martine@outlook.com>
2021-07-01 20:25:08 -0300

    Update links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,19 @@ const Footer = () => {
     <div>
       <footer className={styles.containerFooter}>
         Made by &reg;Diana Martine&trade;
-        <a href=".">
+        <a href="https://github.com/dianamartine">
           <img
             src="https://img.shields.io/badge/-DianaMartine-000?style=for-the-badge&logo=Github&logoColor=white"
             alt="Github"
           />
         </a>
-        <a href=".">
+        <a href="https://linkedin.com/in/dianamartine">
           <img
             src="https://img.shields.io/badge/-Diana_Martine-0077B5?style=for-the-badge&logo=Linkedin&logoColor=white"
             alt="linkedin"
           />
         </a>
-        <a href=".">
+        <a href="https://instagram.com/diana_martine__">
           <img
             src="https://img.shields.io/badge/@diana__martine____-c13584?style=for-the-badge&logo=Instagram&logoColor=white"
             alt="Instagram"
